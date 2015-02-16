3208f310e1d910df902060aaaf8f9cdb5dc619e2
jiffies <lcqtdwj@gmail.com>
2015-02-16 17:03:50 +0800

    blog

---
+++ index.wsgi
@@ -0,0 +1,7 @@
+import sae
+import os
+import sys
+root = os.path.dirname(__file__)
+sys.path.insert(0,os.path.join(root,'site-packages'))
+import wsgi
+application = sae.create_wsgi_app(wsgiapp.application)
--- urls.py
+++ urls.py
@@ -7,9 +7,9 @@ from models import User,Blog
 
 @view('content.html')
 @get('/')
-@with_connection
-def test_blogs():
+def all_blogs():
     blogs = Blog.find_all()
-    users = User.find_all()
-    return dict()
+    main = blogs[0]
+    sub = blogs[1:]
+    return dict(main=main,sub=sub)
 
