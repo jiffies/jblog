9ff1524f59eb9225e36711ffecdf58ec1c7f370c
jiffies <lcqtdwj@gmail.com>
2015-02-16 11:28:39 +0800

    tempalte test

---
+++ static/js/jblog.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+        alert("ready?");
+$(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    var scrollHeight = $(document).height();
+    var windowHeight = $(this).height();
+    if(scrollTop + windowHeight == scrollHeight){
+        $("#fright").css('display','block');
+    }
+    else{
+        $("#fright").css('display','none');
+    }
+});
+
+$("#container").imagesLoaded().always(function(){
+            var container = document.querySelector('#container');
+            var msnry = new Masonry( container, {
+                  columnWidth: ".sub",
+                    itemSelector: '.article'
+                    });
+            $(".article").css('margin-left','4%');
+});
+});
+
